714e67b62fd6346128b27f4e3138bc5528453cae
Thanapong Prathumchat <tanapong21@gmail.com>
2019-04-17 09:24:24 +0700

    Reindent js files

--- webpack.config.js
+++ webpack.config.js
@@ -9,43 +9,43 @@ const PurgecssPlugin = require('purgecss-webpack-plugin');
 //
 // https://github.com/FullHuman/purgecss#extractor
 class TailwindExtractor {
-    static extract(content) {
-        return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-    }
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
 }
 
 module.exports = {
-    entry: './src/styles.css',
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: [
-                        { loader: 'css-loader', options: { importLoader: 1 } },
-                        'postcss-loader'
-                    ],
-                    fallback: 'style-loader'
-                })
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new ExtractTextPlugin('[name].[hash].css'),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-        new PurgecssPlugin({
-            paths: path.resolve(__dirname, 'src/index.html'),
-            extractors: [{
-                extractor: TailwindExtractor,
-                extensions: ['html']
-            }]
+  entry: './src/styles.css',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            { loader: 'css-loader', options: { importLoader: 1 } },
+            'postcss-loader'
+          ],
+          fallback: 'style-loader'
         })
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new PurgecssPlugin({
+      paths: path.resolve(__dirname, 'src/index.html'),
+      extractors: [{
+        extractor: TailwindExtractor,
+        extensions: ['html']
+      }]
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
